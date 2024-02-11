60d812ae60e5b2c2967afee45c1f94ee6a4d4874
AsinShaikh <145040135+AsinShaikh@users.noreply.github.com>
2024-02-11 21:11:17 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,11 @@
+localStorage.setItem("user_name", user_name);
+var firebaseConfig = {
+    apiKey: "AIzaSyAm1jh9NwGCLQTDAJmMd6s5su6jPdPnOck",
+    authDomain: "food-419d3.firebaseapp.com",
+    databaseURL: "https://food-419d3-default-rtdb.firebaseio.com",
+    projectId: "food-419d3",
+    storageBucket: "food-419d3.appspot.com",
+    messagingSenderId: "689565320445",
+    appId: "1:689565320445:web:94bb52722604738f0e2a94"
+  };
+window.location = "chat_room.html";
